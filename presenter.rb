353465bfdfd6e1f486f43cfa51031b0d6abd370e
Juan Carlos <juancarlosenrique103@gmail.com>
2020-11-26 17:39:45 -0500

    Create auxiliar method group_by_categories and select_table for print_categories :zap:

--- presenter.rb
+++ presenter.rb
@@ -12,7 +12,7 @@ module Presenter
     table = Terminal::Table.new
     table.title = incomes ? "Incomes\nNovember 2020" : "Expenses\nNovember 2020"
     table.headings = %w[ID Category Total]
-    table.rows = categories_table.map do |category|
+    table.rows = select_table.map do |category|
       [
         category[:id],
         category[:name],
@@ -20,5 +20,27 @@ module Presenter
       ]
     end
     puts table
+  end
+
+  def group_by_categories(categories)
+    incomes = []
+    expenses = []
+    categories.each do |category|
+      category[:transaction_type] == "income" ? incomes << category : expenses << category
+    end
+    { incomes: incomes, expenses: expenses }
+  end
+
+  def select_table
+    # simula el request categories.index
+    file = File.read("./categories.json")
+    new_categories = JSON.parse(file, symbolize_names: true)
+    new_categories = group_by_categoriesies(new_categories)
+    incomes = false # deberia estar en el initialize @incomes
+    if incomes
+      new_categories[:incomes]
+    else
+      new_categories[:expenses]
+    end
   end
 end
