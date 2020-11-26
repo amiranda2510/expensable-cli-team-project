c08c730de360e656578c20b42f2a35e0b8bb7c3b
Juan Carlos <juancarlosenrique103@gmail.com>
2020-11-26 13:14:26 -0500

    Solved error syntax symbolize_names

--- session_controller.rb
+++ session_controller.rb
@@ -22,6 +22,6 @@ class SessionController
   def raise_and_send_response(response)
     raise Net::HTTPError.new(response.message, response) unless response.success?
 
-    JSON.parse(response.body, symbolize_name: true) if response.body
+    JSON.parse(response.body, symbolize_names: true) if response.body
   end
 end
